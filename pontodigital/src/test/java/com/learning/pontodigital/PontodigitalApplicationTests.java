f20a102f7e4e424af02efbdad8f8864cf2ad590e
VanessaPessoa <vanessalima@cc.ci.ufpb.br>
2021-03-11 18:22:09 -0300

    primeiro commit

---
+++ pontodigital/src/test/java/com/learning/pontodigital/PontodigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.pontodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontodigitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
